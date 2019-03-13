1342533536f2ebb791c259fdab5c844205cce47f
Andrew Mowry <andrew@mowrystrings.com>
2019-03-13 09:43:10 -0700

    Delete WARBL_Serial.h

--- WARBL_9.0/WARBL_Serial.h
+++
@@ -1,6 +0,0 @@
-
-
-//The line below will remove serial communcation to make the device fully class-commpliant and not require drivers on any OS.
-//It can be commented out to use Serial.print or to make it unnecessary to double-click the reset button for programming.
-
-#define CDCCON_DISABLE  
